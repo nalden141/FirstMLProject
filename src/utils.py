023c592e7565a591cfac219f26cdf9f53526948b
nalden141 <noor15511551@gmail.com>
2023-05-30 18:38:07 +0200

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,4 @@
-import os,sys,dill
-
+import os,sys,dill,pickle
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
@@ -46,6 +45,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
